e6099b8d689442e4f0532960ffd050cfc5c068f8
student <student@M11032>
2018-01-30 14:11:53 +0900

    ..........

---
+++ src/tcpip2/Client.java
@@ -0,0 +1,12 @@
+package tcpip2;
+
+public class Client {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+
+	}
+
+}
---
+++ src/tcpip2/Server.java
@@ -0,0 +1,12 @@
+package tcpip2;
+
+public class Server {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+	}
+
+}
