352f6ff5d3d127796bfabaed651ff0588a6bbdd9
student <student@M11032>
2018-01-30 13:39:53 +0900

    dhffu

--- src/tcpip1/Server.java
+++ src/tcpip1/Server.java
@@ -1,5 +1,63 @@
 package tcpip1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
 
+	public static void main(String[] args) {
+		
+		
+		ServerSocket serversocket = null;
+		Socket socket = null;
+		InputStream in= null;
+		InputStreamReader inr = null;
+		BufferedReader br = null;
+		try {
+			serversocket = new ServerSocket(7777);
+			InetAddress ia = InetAddress.getLocalHost();
+			System.out.println(ia.getHostName());
+			System.out.println(ia.getHostAddress());
+			System.out.println("Start Server.........");
+			
+			
+			while(true) {
+				
+		        socket = serversocket.accept();
+				System.out.println("Client Connected...");
+				
+				//Receive Data.....
+				in = socket.getInputStream();
+				inr = new InputStreamReader(in);
+				br = new BufferedReader(inr);
+				String receivestr = br.readLine();
+				System.out.println(receivestr);
+				System.out.println("End Server......");
+
+			}
+				
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				br.close();
+				inr.close();
+				in.close();
+				socket.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		
+	}
+	
+	
+	
 }
